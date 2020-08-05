443eca3971f8a3d143d6a7568667a51c64dce6a9
Gabriel Tagliari Rodrigo <gtagliari@ciandt.com>
2020-08-05 11:12:42 -0300

    (WIP) Creating component weather information

---
+++ src/components/atoms/WeatherInformation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img alt="Vue logo" :src="require('../../assets/' + icon)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherInformation',
+  props: {
+    icon: String
+  },
+  // data () {
+  //   return {
+  //     icon: require('../../assets/logo.png')
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WeatherInformation icon="logo.png"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherInformation from '../components/atoms/WeatherInformation'
 
 export default {
-  name: 'App',
+  name: 'Home',
   components: {
-    HelloWorld
+    WeatherInformation
   }
 }
 </script>
